afc861243b91d466061fab2c1447314ed09cc1c7
System Administrator <root@Haris-MacBook-Pro.local>
2020-02-17 20:43:12 -0800

    Setting up app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "clarity-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://rodhl001x7.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_8y6EyClyy",
+    APP_CLIENT_ID: "594difpegegmp794jasqrcojfk",
+    IDENTITY_POOL_ID: "us-east-2:81d0b6ea-8d43-4a49-a9f2-009e3588815e"
+  }
+};
+
